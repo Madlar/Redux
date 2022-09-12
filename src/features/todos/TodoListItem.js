8ea74bc0f7af59d81d2c4f5b43df1ea80eb6567a
Madlar <pjcj1@naver.com>
2022-09-12 20:32:39 +0900

    Using Redux with React
    Designing the Component Tree

---
+++ src/features/todos/TodoListItem.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+import { ReactComponent as TimeSolid } from './times-solid.svg'
+
+import { availableColors, capitalize } from '../filters/colors'
+
+const TodoListItem = ({ todo, onColorChange, onCompletedChange, onDelete }) => {
+    const { text, completed, color } = todo
+
+    const handleCompletedChange = (e) => {
+        onCompletedChange(e.target.checked)
+    }
+
+    const handleColorChange = (e) => {
+        onColorChange(e.target.value)
+    }
+
+    const colorOptions = availableColors.map((c) => (
+        <option key={c} value={c}>
+            {capitalize(c)}
+        </option>
+    ))
+
+    return (
+        <li>
+            <div className="view">
+                <div className="segment label">
+                    <input
+                        className="toggle"
+                        type="checkbox"
+                        checked={completed}
+                        onChange={handleCompletedChange}
+                    />
+                    <div className="todo-text">{text}</div>
+                </div>
+                <div className="segment buttons">
+                    <select
+                        className="colorPicker"
+                        value={color}
+                        style={{ color }}
+                        onChange={handleColorChange}
+                    >
+                        <option value=""></option>
+                        {colorOptions}
+                    </select>
+                    <button className="destroy" onClick={onDelete}>
+                        <TimeSolid />
+                    </button>
+                </div>
+            </div>
+        </li>
+    )
+}
+
+export default TodoListItem
--- src/index.js
+++ src/index.js
@@ -5,16 +5,7 @@ import App from './App'
 
 import './api/server'
 
-// Omit existing React imports
-
 import store from './store'
-
-store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about actions' })
-// log: '1'
-// log: '2'
-// log: '3'
-
-// Omit existing React rendering logic
 
 ReactDOM.render(
   <React.StrictMode>
