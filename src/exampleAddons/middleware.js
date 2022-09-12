cdff700cabbf9ef7e82016132c186e8833cdd385
Madlar <pjcj1@naver.com>
2022-09-12 13:32:21 +0900

    Middleware
    Redux middleware are written as a series of three nested functions. Let's see what that pattern looks like. We'll start by trying to write this middleware using the function keyword, so that it's more clear what's happening:
    
    Whenever an action is dispatched:
    
    The first part of the handleAction function runs, and we print 'dispatching'
    We pass the action to the next section, which may be another middleware or the real store.dispatch
    Eventually the reducers run and the state is updated, and the next function returns
    We can now call storeAPI.getState() and see what the new state is
    We finish by returning whatever result value came from the next middleware
    Any middleware can return any value, and the return value from the first middleware in the pipeline is actually returned when you call store.dispatch(). For example:
    
    Middleware Use Cases
    So, what can we do with middleware? Lots of things!
    
    A middleware can do anything it wants when it sees a dispatched action:
    
    Log something to the console
    Set timeouts
    Make asynchronous API calls
    Modify the action
    Pause the action or even stop it entirely
    and anything else you can think of.
    
    In particular, middleware are intended to contain logic with side effects. In addition, middleware can modify dispatch to accept things that are not plain action objects. We'll talk more about both of these in Part 6: Async Logic.

--- src/exampleAddons/middleware.js
+++ src/exampleAddons/middleware.js
@@ -12,3 +12,32 @@ export const print3 = (storeAPI) => (next) => (action) => {
   console.log('3')
   return next(action)
 }
+
+export const anotherExampleMiddleware = storeAPI => next => action => {
+  // Do something in here, when each action is dispatched
+
+  return next(action)
+}
+
+export const loggerMiddleware = storeAPI => next => action => {
+  console.log('dispatching', action)
+  let result = next(action)
+  console.log('next state', storeAPI.getState())
+  return result
+}
+
+export const alwaysReturnHelloMiddleware = storeAPI => next => action => {
+  const originalResult = next(action)
+  // Ignore the original result, return something else
+  return 'Hello!'
+}
+
+export const delayedMessageMiddleware = storeAPI => next => action => {
+  if (action.type === 'todos/todoAdded') {
+    setTimeout(() => {
+      console.log('Added a new todo: ', action.payload)
+    }, 1000)
+  }
+
+  return next(action)
+}
