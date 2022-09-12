dc98722350626b870fab16738a324ce22df9b69d
Madlar <pjcj1@naver.com>
2022-09-12 09:29:58 +0900

    Configuring the Store
    We've already seen that we can pass rootReducer and preloadedState arguments to createStore. However, createStore can also take one more argument, which is used to customize the store's abilities and give it new powers.
    
    Redux stores are customized using something called a store enhancer. A store enhancer is like a special version of createStore that adds another layer wrapping around the original Redux store. An enhanced store can then change how the store behaves, by supplying its own versions of the store's dispatch, getState, and subscribe functions instead of the originals.

--- src/store.js
+++ src/store.js
@@ -1,6 +1,9 @@
 import { createStore } from "redux";
 import rootReducer from "./reducer";
+import { sayHiOnDispatch } from './exampleAddons/enhancers'
 
-const store = craeteStore(rootReducer)
+// We don't have a preloadedState value here
+// so we'll pass undefined as the second argument instead.
+const store = createStore(rootReducer, undefined, sayHiOnDispatch)
 
 export default store
